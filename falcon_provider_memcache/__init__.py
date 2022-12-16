516db790825b850a46473e96410e10931d3d4b5a
Bracey Summers <bcsummers@gmail.com>
2022-12-16 10:18:54 -0600

    General cleanup - removed metadata file - updated pre-commit config - added codeql github action for security scanning

--- falcon_provider_memcache/__init__.py
+++ falcon_provider_memcache/__init__.py
@@ -1,12 +1,4 @@
 """Falcon memcache module."""
 # flake8: noqa
-from .__metadata__ import (
-    __author__,
-    __author_email__,
-    __description__,
-    __license__,
-    __package_name__,
-    __url__,
-    __version__,
-)
-from .utils import MemcacheClient, memcache_client
+# first-party
+from falcon_provider_memcache.utils import MemcacheClient, memcache_client
